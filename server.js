52a9992fd710075f7f2d6447d010892508189954
Your Name <you@example.com>
2021-05-28 14:21:23 -0600

    route set up

--- server.js
+++ server.js
@@ -18,12 +18,14 @@ app.use(express.urlencoded({extended: true}))
 // Routes
 app.get('/auth0', async (req, res) => {
 
+    // Fetch apps/clients
     const apps = await axios.get(`${DOMAIN}/api/v2/clients`, {
         headers: { authorization: ACCESS_TOKEN}
     }).then((res) => {
             return res.data
     })
 
+    //Fetch rules
     const rules = await axios.get(`${DOMAIN}/api/v2/rules`, {
         headers: { authorization: ACCESS_TOKEN}
     }).then((res) => {
@@ -31,20 +33,26 @@ app.get('/auth0', async (req, res) => {
             return res.data
     })
 
+    // Init output variable
     const output = {}
 
+    // Loop through one app at a time
     for (let i = 0; i < apps.length; i++) {
+        // Init key/value pairs for each app
         output[apps[i].name] = {}
         output[apps[i].name].rules = []
+        // Loop through each rule and check for app name within the script
         for (let j = 0; j < rules.length; j++) {
+            // If found, push that rule into the rules array of that app
             if (rules[j].script.includes(apps[i].name)) {
                 output[apps[i].name].rules.push(rules[j].name)
             }
         }
     }
-    
-    res.json(output)
 
+    // Return the output
+    res.json(output)
+    
 })
 
 // Listener
